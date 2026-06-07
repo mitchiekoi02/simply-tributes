7f20782d6c29e7de7285c5517b8cb0dcec6546c0
mitchiekoi02 <mymissgee@gmail.com>
2026-06-07 09:06:25 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+const root = document.documentElement;
 
+// COLORS
+root.style.setProperty("--primary-color", data.theme.primaryColor);
+root.style.setProperty("--secondary-color", data.theme.secondaryColor);
+root.style.setProperty("--accent-color", data.theme.accentColor);
+
+// FONTS
+document.body.style.fontFamily = data.theme.bodyFont;
+
+document.querySelector("#hero-name").style.fontFamily = data.theme.headingFont;
+document.querySelector("#hero-degree").style.fontFamily = data.theme.headingFont;
+document.querySelector("#hero-school").style.fontFamily = data.theme.bodyFont;
+document.querySelector("#hero-quote").style.fontFamily = data.theme.bodyFont;
